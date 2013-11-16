5eef9f62d5afe04af8bd3b5c6dd6480d12b48e50
enragedmrt <amosg@knights.ucf.edu>
2013-11-16 13:41:46 -0500

    Drawer listview now expanding instead of drilldown

---
+++ DeviantArt/src/main/java/com/grantamos/android/deviantart/adapter/MultiExpandingListAdapter.java
@@ -0,0 +1,136 @@
+package com.grantamos.android.deviantart.adapter;
+
+import android.app.Activity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.grantamos.android.deviantart.R;
+import com.grantamos.android.deviantart.helpers.CategoryItem;
+import com.grantamos.android.util.KTreeNode;
+
+import java.util.LinkedList;
+import java.util.ListIterator;
+
+/**
+ * Created by Grant on 11/16/13.
+ */
+public class MultiExpandingListAdapter extends BaseAdapter {
+
+    KTreeNode<CategoryItem> mRoot, mSelectedNode;
+    LinkedList<KTreeNode<CategoryItem>> mItems;
+    ListIterator<KTreeNode<CategoryItem>> mIterator;
+    Activity mContext;
+    int mAccentColor, mNormalColor;
+
+    public MultiExpandingListAdapter(Activity context, KTreeNode<CategoryItem> root){
+        mRoot = root;
+        mContext = context;
+
+        mItems = new LinkedList<KTreeNode<CategoryItem>>();
+
+        expandNode(root);
+
+        mAccentColor = mContext.getResources().getColor(R.color.accent);
+        mNormalColor = mContext.getResources().getColor(R.color.white);
+    }
+
+    public void expandNode(KTreeNode<CategoryItem> node){
+
+        int index = mItems.indexOf(node) + 1;
+        node.setIsMarked(true);
+
+        for(KTreeNode<CategoryItem> item : node.children){
+            mItems.add(index, item);
+            index++;
+        }
+
+        mIterator = mItems.listIterator();
+    }
+
+    public void collapseNode(KTreeNode<CategoryItem> node){
+
+        int index = mItems.indexOf(node) + 1;
+        ListIterator listIterator = mItems.listIterator(index);
+        node.setIsMarked(false);
+
+        for(int i = 0; i < node.childCount() && listIterator.hasNext(); i++){
+            KTreeNode<CategoryItem> n = (KTreeNode<CategoryItem>) listIterator.next();
+            if(n.getIsMarked()){
+                collapseNode(n);
+                listIterator = mItems.listIterator(index + i);
+                listIterator.next();
+            }
+            listIterator.remove();
+        }
+
+        mIterator = mItems.listIterator();
+    }
+
+    @Override
+    public int getCount() {
+        return mItems.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        KTreeNode<CategoryItem> ret = null;
+
+        if(i < 0 || i > getCount())
+            return ret;
+
+        while(i >= mIterator.nextIndex() && mIterator.hasNext())
+            ret = mIterator.next();
+
+        while(i <= mIterator.previousIndex() && mIterator.hasPrevious())
+            ret = mIterator.previous();
+
+        return ret;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup viewGroup) {
+
+        ViewHolder viewHolder;
+
+        KTreeNode<CategoryItem> item = (KTreeNode<CategoryItem>) getItem(position);
+
+        if(convertView == null){
+            convertView = mContext.getLayoutInflater().inflate(R.layout.list_item_drawer, viewGroup, false);
+
+            viewHolder = new ViewHolder();
+            viewHolder.textView = (TextView) convertView.findViewById(R.id.text_view);
+            viewHolder.padding = convertView.getPaddingLeft();
+
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        viewHolder.textView.setText(item.getValue().toString());
+
+        if(mSelectedNode == item)
+            viewHolder.textView.setTextColor(mAccentColor);
+        else
+            viewHolder.textView.setTextColor(mNormalColor);
+
+        convertView.setPadding(viewHolder.padding + (item.getDepth() * viewHolder.padding * 2), convertView.getPaddingTop(), convertView.getPaddingRight(), convertView.getPaddingBottom());
+
+        return convertView;
+    }
+
+    public void setSelectedNode(KTreeNode<CategoryItem> selectedNode) {
+        this.mSelectedNode = selectedNode;
+    }
+
+    static class ViewHolder{
+        TextView textView;
+        int padding;
+    }
+}
---
+++ DeviantArt/src/main/java/com/grantamos/android/deviantart/fragment/MultiExpandingListFragment.java
@@ -0,0 +1,105 @@
+package com.grantamos.android.deviantart.fragment;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.ListView;
+
+import com.grantamos.android.deviantart.R;
+import com.grantamos.android.deviantart.activity.BrowseActivityInterface;
+import com.grantamos.android.deviantart.adapter.DrillDownArrayAdapter;
+import com.grantamos.android.deviantart.adapter.MultiExpandingListAdapter;
+import com.grantamos.android.deviantart.helpers.CategoryItem;
+import com.grantamos.android.util.KTreeNode;
+
+/**
+ * Created by Grant on 11/16/13.
+ */
+public class MultiExpandingListFragment extends ListFragment {
+
+    static String TAG = "MultiExpandingListFragment";
+    KTreeNode<CategoryItem> root;
+    BrowseActivityInterface mCallback;
+    MultiExpandingListAdapter mListAdapter;
+
+    public static MultiExpandingListFragment newInstance(KTreeNode<CategoryItem> root){
+
+        MultiExpandingListFragment multiExpandingListFragment = new MultiExpandingListFragment();
+        Bundle args = new Bundle();
+        args.putSerializable("root", root);
+        multiExpandingListFragment.setArguments(args);
+
+        return multiExpandingListFragment;
+    }
+
+    @Override
+    public void setArguments(Bundle args){
+
+        this.root = (KTreeNode<CategoryItem>) args.getSerializable("root");
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+
+        super.onCreate(savedInstanceState);
+
+        if(savedInstanceState != null)
+            this.setArguments(savedInstanceState);
+    }
+
+    @Override
+    public void onAttach(Activity activity){
+
+        super.onAttach(activity);
+
+        try{
+            mCallback = (BrowseActivityInterface) activity;
+        } catch (Exception e){
+            Log.d(TAG, e.toString());
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState){
+
+        super.onSaveInstanceState(outState);
+        outState.putSerializable("root", root);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater layoutInflater, ViewGroup container, Bundle savedInstanceState){
+
+        ListView listView = (ListView) layoutInflater.inflate(R.layout.list_view_drawer, container, false);
+        listView.setChoiceMode(AbsListView.CHOICE_MODE_SINGLE);
+        mListAdapter = new MultiExpandingListAdapter(getActivity(), root);
+        setListAdapter(mListAdapter);
+
+        return listView;
+    }
+
+    @Override
+    public void onListItemClick(ListView listView, View view, int position, long id){
+
+        KTreeNode<CategoryItem> clickedItem = (KTreeNode<CategoryItem>) getListAdapter().getItem(position);
+
+        mCallback.onCategorySelected(clickedItem.getValue());
+        mListAdapter.setSelectedNode(clickedItem);
+
+        if(clickedItem.getIsMarked()){
+            mListAdapter.collapseNode(clickedItem);
+            mListAdapter.notifyDataSetChanged();
+        }
+        else if(clickedItem.childCount() > 0){
+            mListAdapter.expandNode(clickedItem);
+            mListAdapter.notifyDataSetChanged();
+        } else {
+            getListView().invalidateViews();
+            mCallback.dismissDrawer();
+        }
+    }
+}
